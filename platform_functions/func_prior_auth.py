d5b12071967da727407d769aec649cc651c26111
kushagrakukreja <kushagra.kukreja@exponentialai.com>
2025-10-08 19:38:42 +0530

    Update func_prior_auth.py

--- platform_functions/func_prior_auth.py
+++ platform_functions/func_prior_auth.py
@@ -2,7 +2,7 @@ import json
 
 def func_prior_auth(number, config="none"):
     output = []
-    for i in range(1, 13):
+    for i in range(1, 15):
         result = number * i
         output.append({
             "expression": f"{number} x {i}",
