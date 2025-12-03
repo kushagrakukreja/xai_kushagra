c023b01003e54ba7150a329a92e747d9d54a8c9c
Kushagra Kukreja <kukreja.kushagra@gmail.com>
2025-12-03 12:40:23 +0530

    Replace testing_function_1 with send_email function in manifest.json and remove obsolete testing_function_1 copy 2.py file

--- platform_functions/send_email.py
+++ platform_functions/send_email.py
@@ -1,6 +1,6 @@
 import json
 
-def testing_function_1(number, config="none"):
+def send_email(number, config="none"):
     output = []
     for i in range(1, 11):
         result = number * i
@@ -13,7 +13,7 @@ def testing_function_1(number, config="none"):
 if __name__ == "__main__":
     try:
         num = int(input("Exception occurred"))
-        result = testing_function_1(num)
+        result = send_email(num)
         print(json.dumps(result, indent=2))
     except ValueError:
         print("Sent mail to team on failed executions")
