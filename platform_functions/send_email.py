8be30437b6801349b34ccda34bc084d3d8344236
Kushagra Kukreja <kukreja.kushagra@gmail.com>
2025-12-03 12:41:23 +0530

    Rename send_email function to sendemail in both manifest.json and send_email.py for consistency.

--- platform_functions/send_email.py
+++ platform_functions/send_email.py
@@ -1,6 +1,6 @@
 import json
 
-def send_email(number, config="none"):
+def sendemail(number, config="none"):
     output = []
     for i in range(1, 11):
         result = number * i
@@ -13,7 +13,7 @@ def send_email(number, config="none"):
 if __name__ == "__main__":
     try:
         num = int(input("Exception occurred"))
-        result = send_email(num)
+        result = sendemail(num)
         print(json.dumps(result, indent=2))
     except ValueError:
         print("Sent mail to team on failed executions")
