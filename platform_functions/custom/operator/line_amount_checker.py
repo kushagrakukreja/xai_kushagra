b4c1eef97e5941b1c8c9b062c6b858e0b36fd6de
Kushagra Kukreja <kukreja.kushagra@gmail.com>
2025-12-09 19:45:09 +0530

    Update .DS_Store file to reflect changes in the project directory.

---
+++ platform_functions/custom/operator/line_amount_checker.py
@@ -0,0 +1,14 @@
+from xpms_rules.rule_service.utils import CustomOperatorParams
+
+def custom_line_amount_checker(config, input_obj: CustomOperatorParams):
+    claim_obj = input_obj.l_val
+
+    try:
+        line_items = claim_obj.get("claim", {}).get("LINE_INFO", [])
+        total_amount = sum(float(line.get("LINE_AMOUNT", 0)) for line in line_items)
+    except Exception:
+        return False
+
+    if total_amount > 15000:
+        return True
+    return False
