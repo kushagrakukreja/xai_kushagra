504a7dbf8a0d66bc082aa96776bb20097a8139d2
Sreehanth S <121183862+sreeshanth9@users.noreply.github.com>
2025-12-09 14:49:48 +0530

    Update client_ruleset_execution.py

--- platform_functions/client_ruleset_execution.py
+++ platform_functions/client_ruleset_execution.py
@@ -4,14 +4,14 @@ import requests
 from xpms_rules.models.rules import Ruleset
 
 
-def client_ruleset_execution(payload, **kwargs):
+def client_ruleset_execution(**kwargs):
     client_name = kwargs.get("Client_Name")
     if not client_name:
         msg = "Please provide key Client_Name in payload"
         raise Exception(msg)
 
-    agent_id = payload.get("context").get("agent_id")
-    solution_id = payload.get("context").get("solution_id")
+    agent_id = kwargs.get("config").get("context").get("agent_id")
+    solution_id = kwargs.get("config").get("context").get("solution_id")
 
     ruleset_filter = {
         "agent_id": agent_id,
@@ -25,17 +25,18 @@ def client_ruleset_execution(payload, **kwargs):
     run_all_exclusions = kwargs.get("run_all_exclusions", False)
     execution_method = kwargs.get("execution_method", "Sequential")
 
+    kwargs.pop("Client_Name")
+    kwargs.pop("config")
     kwargs.update({
         "run_all_exclusions": run_all_exclusions,
         "ruleset_ids": ruleset_ids,
         "execution_method": execution_method,
     })
-    del kwargs["Client_Name"]
     inp_payload = {
         "data": {
             **kwargs
         },
-        "solution_id": "rulesenginedemo"
+        "solution_id": "sol31"
     }
 
     requests.post(f"http://{os.getenv('API_GATEWAY_URL')}/execute_ruleset", json=inp_payload)  # noqa: S113
