c47b71942d885eca5cad0bfa703d6d8967e456d7
Kushagra Kukreja <kukreja.kushagra@gmail.com>
2026-03-16 12:38:46 +0530

    Update GEMINI_API_KEY in func_prior_auth to a new value for API access.

--- platform_functions/func_prior_auth.py
+++ platform_functions/func_prior_auth.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-GEMINI_API_KEY = "AIzaSyBNWtSErzZGz7o9M9Tggj0LW_uG3uAAfyc"
+GEMINI_API_KEY = "AIzaSyBf_1aXOZjgf1fcEIDXy4_LewYrV5R75pg"
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
 def func_prior_auth(config, **input_obj) -> dict:
